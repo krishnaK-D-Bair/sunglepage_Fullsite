7a85bcc73ba1d5cbf934fb783349add37d7bf519
krishnaK-D-Bair <krishna.kdb@yahoo.com>
2024-08-06 20:08:58 +0530

    singlePage

---
+++ app.js
@@ -0,0 +1,26 @@
+document.querySelector('.icon_in1').addEventListener('click' , () => {
+    document.querySelector('.trigger_1').classList.toggle('active');
+})
+
+console.log('hello world')
+
+document.querySelector('.icon_in2').addEventListener('click' , () => {
+    document.querySelector('.trigger_2').classList.toggle('active');
+})
+document.querySelector('.icon_in3').addEventListener('click' , () => {
+    document.querySelector('.trigger_3').classList.toggle('active');
+})
+document.querySelector('.icon_in4').addEventListener('click' , () => {
+    document.querySelector('.trigger_4').classList.toggle('active');
+})
+document.querySelector('.icon_in5').addEventListener('click' , () => {
+    document.querySelector('.trigger_5').classList.toggle('active');
+})
+
+
+document.querySelector('#nav_icon').addEventListener('click' , () => {
+    document.querySelector('.main_ul').classList.toggle('active_ul');
+  
+    
+})
+
